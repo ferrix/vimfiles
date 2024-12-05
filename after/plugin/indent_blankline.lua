91ba8d4615a744c9636b1f72e650d01309a3b512
Ferrix Hovi <ferrix@ferrix.fi>
2024-12-05 09:40:12 +0200

    Upgrade ibl

--- after/plugin/indent_blankline.lua
+++ after/plugin/indent_blankline.lua
@@ -1,6 +1,6 @@
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
+require('ibl').setup {
+  indent = { char = '┊', },
+  whitespace = { remove_blankline_trail = true, },
 }
